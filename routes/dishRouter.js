5c8352358b40626eb3393d68feba6df446fd6b2f
Prashanth <yvprashanth@gmail.com>
2019-05-05 20:37:08 -0700

    Slightly more

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -21,17 +21,36 @@ dishRouter.route('/')
 .get((req, res, next) => {
     connect.then((db) => {
         console.log('Connected correctly to server');
+
         Dishes.create({
-            name: 'Uthapizza',
-            description: 'Test'
+            name: 'Uthappizza',
+            description: 'test'
         })
         .then((dish) => {
             console.log(dish);
-            return Dishes.find({}).exec();
+    
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: { description: 'Updated test'}
+            },{ 
+                new: true 
+            })
+            .exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
-            // return Dishes.remove({});
+        .then((dish) => {
+            console.log(dish);
+    
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+    
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+    
+            return Dishes.remove({});
         })
         .then(() => {
             return mongoose.connection.close();
